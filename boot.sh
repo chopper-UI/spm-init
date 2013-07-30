a3738c67f9c8f00593051b617bfc638b3be311e6
Alvin Xu <alvinxwt@gmail.com>
2013-07-30 20:53:51 +0800

    update boot.sh

--- boot.sh
+++ boot.sh
@@ -5,11 +5,18 @@ THEME="$HOME/.spm/init/chopper"
 
 cd $CWD
 
+if ! which spm > /dev/null; then
+    echo "install spm"
+    npm install spm -g
+else
+    echo "you have installed spm"
+fi
+
 if [ ! -d $THEME ]; then
-    echo "clone nico chopper init"
+    echo "clone spm chopper init"
     git clone https://github.com/chopper-UI/spm-init.git $THEME
 else
-    echo "update nico chopper init"
+    echo "update spm chopper init"
     cd $THEME
     git pull origin master
     cd $CWD
